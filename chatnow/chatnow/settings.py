2b90490d2a5859e441df5f2cd54f82062f2a32c1
VerbalThree <verbalthree00@gmail.com>
2024-10-29 21:35:44 -0300

    Creation of Database

--- chatnow/chatnow/settings.py
+++ chatnow/chatnow/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chatnow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
